dd249a59b6ebeca5f327617d169cff741250cfad
Jesse Johnson <akpene@Jesses-MacBook-Pro.local>
2020-05-22 16:08:17 +0000

    Clean up code

--- app/src/main/java/com/jessejojojohnson/quottable/MainActivity.kt
+++ app/src/main/java/com/jessejojojohnson/quottable/MainActivity.kt
@@ -2,13 +2,12 @@ package com.jessejojojohnson.quottable
 
 import android.content.Intent
 import android.graphics.Bitmap
-import android.graphics.Canvas
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.MediaStore
 import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
 import androidx.core.view.drawToBitmap
 import kotlinx.android.synthetic.main.activity_main.*
@@ -24,8 +23,7 @@ class MainActivity : AppCompatActivity() {
 
         ivShare.setOnClickListener(object: View.OnClickListener{
             override fun onClick(v: View?) {
-                var bitmap = clImageQuote.drawToBitmap()
-                var uri = saveImage(bitmap)
+                val uri = saveImage(clImageQuote.drawToBitmap())
                 val intent = Intent(Intent.ACTION_SEND)
                 intent.putExtra(Intent.EXTRA_STREAM, uri)
                 intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
@@ -36,20 +34,22 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun saveImage(image: Bitmap): Uri?{
-        var imagesFolder = File(cacheDir, "images")
         var uri: Uri? = null
         try {
+            val imagesFolder = File(cacheDir, "images")
             imagesFolder.mkdirs()
-            var file = File(imagesFolder,
+            val file = File(imagesFolder,
                 "quottable-${System.currentTimeMillis()}.png")
-            var stream = FileOutputStream(file)
+            val stream = FileOutputStream(file)
             image.compress(Bitmap.CompressFormat.PNG, 90, stream)
             stream.flush()
             stream.close()
             uri = FileProvider.getUriForFile(this,
                 "com.jessejojojohnson.fileprovider", file)
         }catch (e: IOException){
-            Log.e("qtb",e.localizedMessage )
+            Toast.makeText(this,
+                "Quottable could not create the file! ☹️",
+                Toast.LENGTH_SHORT).show()
         }
         return uri
     }
