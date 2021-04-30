4105f60dc227b1afdf18086ace73246ef4a9ed8d
Jesse Johnson <=>
2021-04-30 08:49:55 +0000

    Redesign quote

--- app/src/main/java/com/jessejojojohnson/quottable/MainActivity.kt
+++ app/src/main/java/com/jessejojojohnson/quottable/MainActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
+/*
+* We'll leave MainActivity open for future modification
+* and have the app proper in QuoteActivity*/
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
