0ff43b5796384faae2d561112a1c92460c4ad492
Jesse Johnson <=>
2021-09-05 17:03:47 +0000

    Remove redundant SDK version check

--- app/src/main/java/com/jessejojojohnson/quottable/Util.kt
+++ app/src/main/java/com/jessejojojohnson/quottable/Util.kt
@@ -7,11 +7,11 @@ import android.os.Build
 import androidx.core.app.ActivityCompat
 
 fun hasPermissions(context: Context?, vararg permissions: String?): Boolean{
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && context != null){
-        for (permission in permissions){
-            if (ActivityCompat.checkSelfPermission(context, permission!!) != PackageManager.PERMISSION_GRANTED){
-                return false
-            }
+    for (permission in permissions){
+        if (context?.let {
+                ActivityCompat.checkSelfPermission(it, permission!!)
+        } != PackageManager.PERMISSION_GRANTED){
+            return false
         }
     }
     return true
