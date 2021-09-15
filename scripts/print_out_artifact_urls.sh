1534bc81ecc191df69fc0018449c175bcb619bf4
Jesse Johnson <=>
2021-09-15 20:33:27 +0000

    Clean up print_out_artifact_urls.sh

--- scripts/print_out_artifact_urls.sh
+++ scripts/print_out_artifact_urls.sh
@@ -9,7 +9,7 @@ the_url="$bitrise_api_url/apps/$app_slug/builds/$build_slug/artifacts"
 
 artifacts_array=$(curl -s -H "Authorization: $access_token" $the_url)
 
-for artifact_index in 0 #1 2 #we have 3 artifacts
+for artifact_index in 0
 do
     artifact=$(echo ${artifacts_array} | jq -r ".data[$artifact_index]")
 
@@ -20,8 +20,5 @@ do
     artifact_metadata_response=$(curl -s -H "Authorization: $access_token" ${artifact_url})
     artifact_name=$(echo ${artifact_metadata_response} | jq -r '.data .title')
     artifact_download_url=$(echo ${artifact_metadata_response} | jq -r '.data .expiring_download_url')
-#    echo
-#    echo ":point_down: $artifact_name :point_down:"
-#    echo
     echo ${artifact_download_url}
 done
